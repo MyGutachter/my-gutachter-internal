c94049962c11af0a015788738d49733f2ebbe29d
Parth Solanki <parthsolanki049@gmail.com>
2026-08-14 15:26:33 +0530

    Add ServletServerContainerFactoryBean

--- backend/src/main/java/com/mygutachter/config/WebSocketConfig.java
+++ backend/src/main/java/com/mygutachter/config/WebSocketConfig.java
@@ -1,9 +1,11 @@
 package com.mygutachter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 import com.mygutachter.controller.SignalingHandler;
 
@@ -30,5 +32,13 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(signalingHandler, "/signal")
                 .setAllowedOriginPatterns("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean c = new ServletServerContainerFactoryBean();
+        c.setMaxTextMessageBufferSize(262144);   // 256 KB (was ~8 KB default)
+        c.setMaxBinaryMessageBufferSize(262144);
+        return c;
     }
 }
